c9263a4f365c4b427f10d0951035878449ea9749
red <red.velasco.jr@gmail.com>
2021-04-20 20:28:01 +0800

    Typescript trial
    Typescript trial

--- custom.js
+++ custom.js
@@ -2,21 +2,38 @@
 
 
 
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
 
+window.onscroll = function() {scrollFunction()};
+window.onload  = function() {addUnicodeMenu()};
+
+// Set.prototype.add();
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 function scrollFunction() {
+
+	var navbarKiko = document.getElementById("navbarkiko");
+
   	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 
-    	document.getElementById("navbarkiko").style.padding = "10px 20px";
-    	document.getElementById("navbarkiko").style.background = "#1B527C";
-    	document.getElementById("navbarkiko").style.marginTop = "0";
+    	navbarKiko.style.padding = "10px 20px";
+    	navbarKiko.style.background = "#1B527C";
+    	navbarKiko.style.marginTop = "0";
 
  	} else {
  		
     	// document.getElementById("navbarkiko").style.padding = "80px 10px";
-    	document.getElementById("navbarkiko").style.background = "transparent";
-    	document.getElementById("navbarkiko").style.marginTop = "5vh";
+    	navbarKiko.style.background = "transparent";
+    	navbarKiko.style.marginTop = "5vh";
     	
   	}
 }
+
+function addUnicodeMenu() {
+	var menuItem = document.getElementsByClassName("menu-item-has-children"); 
+
+	if ($("li").hasClass("menu-item-has-children")){
+		menuItem.append("done");
+	}
+}
+
+
